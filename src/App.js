d211a8fa1692c2eb04f0ee79e0d6fe31aebe32ef
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-03-09 15:31:21 +0200

    initial fetching of data

--- src/App.js
+++ src/App.js
@@ -4,27 +4,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super(); 
+
+    this.state = {
+     monsters: [],
+    }
+  }
+
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(users => {
+        this.setState(() => {
+          return {monsters: users}
+        }, () => {console.log(this.state)})
+      })
+  }
   
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map((x) => { 
+            return <div key={x.id}>
+              <h1>{x.name}</h1>
+            </div>
+          })
+        }        
       </div>
     );
   }
 }
 
 export default App;
+
+//jsonplaceholder.typicode.com/users
